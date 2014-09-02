6194ae16de34fd3b3f33b34f9b2925c71980c558
Vedran <vedran@blockchain.info>
2014-09-02 16:37:30 +0900

    CreateWallet API

---
+++ blockchain/createwallet.py
@@ -0,0 +1,27 @@
+"""
+TODO
+"""
+import util
+import json
+ 
+def create_wallet(self, password, api_code, priv = None, label = None, email = None):
+        params = { 'password': password, 'api_code': api_code }
+        if priv is not None:
+            params['priv'] = priv
+        if label is not None:
+            params['label'] = label
+        if email is not None:
+            params['email'] = email
+        
+        response = util.call_api("api/v2/create_wallet", params)
+        json_response = json.loads(response)
+        return CreateWalletResponse(json_response['guid'],
+                                    json_response['address'],
+                                    json_response['link'])
+            
+class CreateWalletResponse:
+    
+    def __init__(self, identifier, address, link):
+        self.identifier = identifier
+        self.address = address
+        self.link = link
