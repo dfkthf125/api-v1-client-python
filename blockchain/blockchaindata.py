3da3be1812002a5f0ba33deb9346090dea4ef32c
Vedran <vedran@blockchain.info>
2014-09-06 20:07:05 +0900

    More endpoints; refactoring

--- blockchain/blockchaindata.py
+++ blockchain/blockchaindata.py
@@ -5,7 +5,6 @@ def get_block(block_id, api_code = None):
     resource = 'rawblock/' + block_id
     if api_code is not None:
         resource += '?api_code=' + api_code
-        
     response = util.call_api(resource)
     json_response = json.loads(response)
     return parse_block(json_response)
@@ -14,76 +13,153 @@ def get_tx(tx, api_code = None):
     resource = 'rawtx/' + tx
     if api_code is not None:
         resource += '?api_code=' + api_code
+    response = util.call_api(resource)
+    json_response = json.loads(response)
+    return Transaction(json_response)
+
+def get_block_height(height, api_code = None):
+    resource = 'block-height/{0}?format=json'.format(height)
+    if api_code is not None:
+        resource += '&api_code=' + api_code
+    response = util.call_api(resource)
+    json_response = json.loads(response)
+    return [parse_block(b) for b in json_response['blocks']]
+
+def get_address(address, api_code = None):
+    resource = 'rawaddr/' + address
+    if api_code is not None:
+        resource += '?api_code=' + api_code
+    response = util.call_api(resource)
+    json_response = json.loads(response)
+    return Address(json_response)
+    
+def get_unspent_outputs(address, api_code = None):
+    resource = 'unspent?active=' + address
+    if api_code is not None:
+        resource += '&api_code=' + api_code
+    response = util.call_api(resource)
+    json_response = json.loads(response)
+    return [UnspentOutput(o) for o in json_response['unspent_outputs']]
+
+def get_latest_block(api_code = None):
+    resource = 'latestblock'
+    if api_code is not None:
+        resource += '?api_code=' + api_code
+    response = util.call_api(resource)
+    json_response = json.loads(response)
+    return LatestBlock(json_response)
+    
+def get_unconfirmed_tx(api_code = None):
+    resource = 'unconfirmed-transactions?format=json'
+    if api_code is not None:
+        resource += '&api_code=' + api_code
+    response = util.call_api(resource)
+    json_response = json.loads(response)
+    return [Transaction(t) for t in json_response['txs']]
+
+def get_blocks(time = None, pool_name = None, api_code = None):
+    resource = 'blocks/{0}?format=json'
+    if api_code is not None:
+        resource += '&api_code=' + api_code
+    if time is not None:
+        resource = resource.format(time)
+    elif pool_name is not None:
+        resource = resource.format(pool_name)
+    else:
+        resource = resource.format('')
         
     response = util.call_api(resource)
     json_response = json.loads(response)
-    return parse_tx(json_response)
-    
-def parse_input(i):
-    obj = i.get('prev_out')
-    if obj is not None:
-        # regular TX
-        return Input(obj['n'], obj['value'], obj['addr'], obj['tx_index'], obj['type'],
-                    obj['script'], i['script'], i['sequence'])
-    else:
-        # coinbase TX
-        return Input(None, None, None, None, None, None, i['script'], i['sequence'])
-    
-def parse_output(o):
-    return Output(o['n'], o['value'], o['addr'], o['tx_index'], o['type'], o['script'], o['spent'])
-
-def parse_tx(t):
-    ins = [parse_input(i) for i in t['inputs']]
-    outs = [parse_output(o) for o in t['out']]
-    return Transaction(t.get('double_spend', False), t.get('block_height'), t['time'], t['vout_sz'], t['vin_sz'],
-                        t['relayed_by'], t['hash'], t['tx_index'], t['ver'], t['size'], ins, outs)
+    return [SimpleBlock(b) for b in json_response['blocks']]
 
 def parse_block(b):
     block_height = b['height']
-    txs = [parse_tx(t) for t in b['tx']]
+    txs = [Transaction(t) for t in b['tx']]
     for tx in txs:
         tx.block_height = block_height
     return Block(b['hash'], b['ver'], b['prev_block'], b['mrkl_root'], b['time'], b['bits'],
                     b['fee'], b['nonce'], b['n_tx'], b['size'], b['block_index'], b['main_chain'],
-                    b['height'], b['received_time'], b['relayed_by'], txs)
-    
+                    b['height'], b.get('received_time'), b.get('relayed_by'), txs)
+
+                    
+class SimpleBlock:
+    def __init__(self, b):
+        self.height = b['height']
+        self.hash = b['hash']
+        self.time = b['time']
+        self.main_chain = b['main_chain']
+        
+class LatestBlock:
+    def __init__(self, b):
+        self.hash = b['hash']
+        self.time = b['time']
+        self.block_index = b['block_index']
+        self.height = b['height']
+        self.tx_indexes = [i for i in b['txIndexes']]
+            
+class UnspentOutput:
+    def __init__(self, o):
+        self.tx_hash = o['tx_hash']
+        self.tx_index = o['tx_index']
+        self.tx_output_n = o['tx_output_n']
+        self.script = o['script']
+        self.value = o['value']
+        self.value_hex = o['value_hex']
+        self.confirmations = o['confirmations']
+                    
+class Address:
+    def __init__(self, a):
+        self.hash160 = a['hash160']
+        self.address = a['address']
+        self.n_tx = a['n_tx']
+        self.total_received = a['total_received']
+        self.total_sent = a['total_sent']
+        self.final_balance = a['final_balance']
+        self.transactions = [Transaction(tx) for tx in a['txs']]
+                    
 class Input:
-        def __init__(self, n, value, address, tx_index, type, script, script_sig, sequence):
-            self.n = n
-            self.value = value
-            self.address = address
-            self.tx_index = tx_index
-            self.type = type
-            self.script = script
-            self.script_sig = script_sig
-            self.sequence = sequence
+    def __init__(self, i):
+        obj = i.get('prev_out')
+        if obj is not None:
+        # regular TX
+            self.n = obj['n']
+            self.value = obj['value']
+            self.address = obj['addr']
+            self.tx_index = obj['tx_index']
+            self.type = obj['type']
+            self.script = obj['script']
+            self.script_sig = i['script']
+            self.sequence = i['sequence']
+        else:
+        # coinbase TX
+            self.script_sig = i['script']
+            self.sequence = i['sequence']
 
 class Output:
-        def __init__(self, n, value, address, tx_index, type, script, spent):
-            self.n = n
-            self.value = value
-            self.address = address
-            self.tx_index = tx_index
-            self.type = type
-            self.script = script
-            self.spent = spent
-        
+    def __init__(self, o):
+        self.n = o['n']
+        self.value = o['value']
+        self.address = o['addr']
+        self.tx_index = o['tx_index']
+        self.type = o['type']
+        self.script = o['script']
+        self.spent = o['spent']
+
 class Transaction:
-    def __init__(self, double_spend, block_height, time, vout_sz, vin_sz,
-                    relayed_by, hash, tx_index, version, size, inputs, outputs):
-        self.double_spend = double_spend
-        self.block_height = block_height
-        self.time = time
-        self.vout_sz = vout_sz
-        self.vin_sz = vin_sz
-        self.relayed_by = relayed_by
-        self.hash = hash
-        self.tx_index = tx_index
-        self.version = version
-        self.size = size
-        self.inputs = inputs
-        self.outputs = outputs
-        
+    def __init__(self, t):
+        self.double_spend = t.get('double_spend', False)
+        self.block_height = t.get('block_height')
+        self.time = t['time']
+        self.vout_sz = t['vout_sz']
+        self.vin_sz = t['vin_sz']
+        self.relayed_by = t['relayed_by']
+        self.hash = t['hash']
+        self.tx_index = t['tx_index']
+        self.version = t['ver']
+        self.size = t['size']
+        self.inputs = [Input(i) for i in t['inputs']]
+        self.outputs = [Output(o) for o in t['out']]
+    
 class Block:
     def __init__(self, hash, version, previous_block, merkle_root, time,
                     bits, fee, nonce, n_tx, size, block_index, main_chain,
@@ -101,7 +177,7 @@ class Block:
         self.block_index = block_index
         self.main_chain = main_chain
         self.height = height
-        self.received_time = received_time
+        self.received_time = received_time if received_time is not None else time
         self.relayed_by = relayed_by
         self.transactions = transactions
         
